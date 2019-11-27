5aedff3ef69292416c8425d453e7833d226494c8
Georgiana Cadar <cadar42@gmail.com>
2019-11-27 17:20:17 +0200

    Stage - Business Logic

---
+++ API/UCM.Business/Stage/IStageService.cs
@@ -0,0 +1,12 @@
+﻿using UCM.Business.Generics;
+using UCM.Business.Stage.Models;
+using System.Threading.Tasks;
+
+namespace UCM.Business.Stage
+{
+    public interface IStageService : IDetailsService<StageDetailsModel>,
+        ICreateService<StageCreateModel>
+    {
+        Task<StageDetailsModel> GetLastStage(params string[] includes);
+    }
+}
