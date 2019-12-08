7d3a73ffc0ef01e10171d49dfea735dbaecb3829
Cada Georgiana <cadar42@gmail.com>
2019-12-08 23:00:23 +0200

    Revert "Revert "Controlers making and HostelStatusService improving""
    This reverts commit 83e23d9859f7fbcb76766fa9e93be9d85506dd5d.

---
+++ API/UCM.Api/Controllers/FilesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UCM.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+    }
+}
